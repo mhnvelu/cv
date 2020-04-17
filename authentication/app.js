09ffe55beae981247bd861a8a52112411333ebc9
Mohanavelu Subramanian <mhnvelu@gmail.com>
2020-04-17 16:01:19 +0100

    Level 4 authentication: email and password salted and hashed using bcrypt

--- authentication/app.js
+++ authentication/app.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
 
 const app = express();
@@ -21,10 +21,12 @@ mongoose.connect("mongodb://localhost:27017/userDB", {
 });
 
 // Level 1 authentication - using email id and password as plaintext
-// Level 2 authentication - using email id and password encrypted
+// Level 2 authentication - using email id and password encrypted using mongoose-encryption
 // Refer https://www.npmjs.com/package/mongoose-encryption
-// Level 3 authentication - using email id and password hashed
+// Level 3 authentication - using email id and password hashed using md5
 // Refer https://www.npmjs.com/package/md5
+// Level 4 authentication - using email id and password salted and hashed using bcrypt
+// Refer https://www.npmjs.com/package/bcrypt
 
 const userSchema = new mongoose.Schema({
     email: String,
@@ -46,18 +48,19 @@ app.get('/register', function (req, res) {
 });
 
 app.post('/register', function (req, res) {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
+    bcrypt.hash(req.body.password, 10, function (err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save(function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render('secrets');
+            }
+        });
     });
-    newUser.save(function (err) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render('secrets');
-        }
-    });
-
 });
 
 app.get('/login', function (req, res) {
@@ -67,15 +70,20 @@ app.get('/login', function (req, res) {
 app.post('/login', function (req, res) {
 
     const email = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
     User.findOne({
         email: email
     }, function (err, foundUser) {
         if (err) {
             console.log(err);
+            res.redirect('/login');
         } else {
-            if (foundUser && foundUser.password === password) {
-                res.render('secrets');
+            if (foundUser) {
+                bcrypt.compare(password, foundUser.password, function (err, result) {
+                    if (result) {
+                        res.render('secrets');
+                    }
+                });
             } else {
                 res.redirect('/login');
             }
