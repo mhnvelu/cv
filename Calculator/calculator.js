1126833a56b3be60830a5869ddf103d5619565a7
Mohanavelu Subramanian <mhnvelu@gmail.com>
2020-04-19 12:33:33 +0100

    Basic calculator, BMI calculator : Implemented using express

---
+++ Calculator/calculator.js
@@ -0,0 +1,35 @@
+//jshint esversion:6
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+// To parse form data
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.listen(3000, function () {
+    console.log("Server is running on Port 3000");
+});
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var result = num1 + num2;
+    res.send("The Result is " + result);
+});
+
+app.get("/bmicalculator", function (req, res) {
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function (req, res) {
+    var weight = Number(req.body.weight);
+    var height = Number(req.body.height);
+    var result = weight / (height * height);
+    res.send("Your BMI is " + result);
+});
